df48711e8e68721593ed1c16c23765ede175f882
Hemalatha T <161028990+shreeselvan@users.noreply.github.com>
2024-08-01 23:36:10 +0530

    Add files via upload
    Implemented the user registration form and backend logic

---
+++ crud/operations/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Employee(models.Model):
+     name = models.CharField(max_length=30)
+     email = models.EmailField()
+     address = models.CharField(max_length=200)
+     def __str__(self):
+         return self.name 
